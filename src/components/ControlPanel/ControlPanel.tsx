d5549355df047b9265a08513aa0e8efc5c6d3d57
Connor Streng <cstreng95@gmail.com>
2023-02-26 10:09:43 +0100

    Create ControlPanel Component

---
+++ src/components/ControlPanel/ControlPanel.tsx
@@ -0,0 +1,25 @@
+import { Children, FunctionComponent, PropsWithChildren } from "react";
+import clsx from "clsx";
+import { BaseComponentProps } from "../../types/BaseComponentProps";
+import "./styles.css";
+
+type Props = BaseComponentProps & {};
+
+const ControlPanel: FunctionComponent<PropsWithChildren<Props>> = ({
+  children,
+  className,
+}) => {
+  return (
+    <ul className={clsx("ControlPanel", className)}>
+      {Children.map(children, (child, index) => {
+        return (
+          <li key={index} className="child">
+            {child}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default ControlPanel;
