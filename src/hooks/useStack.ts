9deecee42705553d2b830682e04f61a60c860ab3
Connor Streng <cstreng95@gmail.com>
2023-03-02 09:08:05 +0100

    Add useStack hook

---
+++ src/hooks/useStack.ts
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+type Stack<Frame> = {
+  topFrame: Frame | undefined;
+  push: (frame: Frame) => void;
+  pop: () => void;
+  clear: () => void;
+};
+
+const useStack = <Frame>(): Stack<Frame> => {
+  const [stack, setStack] = useState<Array<Frame>>([]);
+
+  const push = (frame: Frame) => {
+    setStack((prevStack) => [...prevStack, frame]);
+  };
+
+  const pop = () => {
+    setStack((prevStack) => prevStack.slice(0, prevStack.length - 1));
+  };
+
+  const clear = () => {
+    setStack([]);
+  };
+
+  const topFrame = stack.length > 0 ? stack[stack.length - 1] : undefined;
+
+  return { topFrame, push, pop, clear };
+};
+
+export default useStack;
