51187330d1a0a32aa75e644e09a04391f31863d7
Connor Streng <cstreng95@gmail.com>
2023-03-15 08:55:31 +0100

    Delete unused type

--- src/types/MetroMap.ts
+++
@@ -1,9 +0,0 @@
-import Line from "../classes/Line";
-import LineSegment from "../classes/LineSegment";
-import Station from "../classes/Station";
-
-export type MetroMap = {
-  stations: Array<Station>;
-  lineSegments: Array<LineSegment>;
-  lines: Array<Line>;
-};
